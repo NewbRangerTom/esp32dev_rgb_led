63e0e2eb481a419cc559c77323e93a64007d138b
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2022-05-06 14:14:55 -0400

    moved functions to appropriate header files for better organization.

--- src/main.cpp
+++ src/main.cpp
@@ -41,13 +41,13 @@ CRGB h_LEDs[NUM_LEDS]   = {0};         // Frame buffer for FastLED
 
 int h_Brightness         = 96;         //  brightness range 0 - 255  !!! CAUTION: setting to 255 could have negative effects if underpowered
 int h_PowerLimit         = 15000;      //  1000mW = 1W Power Limit   !!! SEE CAUTION IN HEADER BLOCK. I have started using a 5V-10A-50W power supply.
-const int num_funcs      = 18;
 
 uint8_t initialHue       = 0;
 const uint8_t deltaHue   = 16;
 const uint8_t hueDensity = 4;
 
 // LED effect headers - User defined headers
+
 #include "ledgfx.h"
 #include "bounce.h"
 #include "comet.h"      // *See caution # 1.
@@ -64,15 +64,11 @@ FireEffect fire(NUM_LEDS, 15, 100, 3, 4, true, true);             // reversed an
 PurpleRainEffect rain(NUM_LEDS, 15, 100, 3, 4, true, true);       // f-f = end -> 0 : t-f = 0 -> end : f-t = center -> out : t-t = ends -> center
 BluetoothSerial ESP_BT;
 
+// --- User defined function(s) or function prototype(s) -------------------------
 
-// --- User defined function prototypes -------------------------
-
-void clearLEDs();
-void firstLED();
-void Marquee();
-void MyFire();
-void MyIceFire();
-void PurpleR();
+void Rainbow(){
+  fill_rainbow(h_LEDs, NUM_LEDS, initialHue += hueDensity, deltaHue);
+};
 
 // -------------------------------------------------
 
@@ -97,94 +93,34 @@ void setup() {
 void loop() {
 
   while (true){
+    
+    // byte btReadNew = ESP_BT.read();
 
-	//----------------------------------------------------------------------------------------------------
     // LED strip patern functions
     // uncomment the effect to use
 
-    // Marquee();                                          // see annotations in marquee.h for options
-    // DrawComet();
-    // DrawCometGfx();
-    // DrawComet3();
-    // balls.Draw();                                       //  Bouncing balls using BouncingBallEffect from bounce.h; comment out to use different effect
-    // DrawMarquee();
-    // DrawPixels(h_LEDs, NUM_LEDS, CRGB::Green);
-    // DrawTwinkle();
-    // DrawPurpleTwinkle();
-    // fill_solid(h_LEDs, NUM_LEDS, CRGB::Plaid);
-    // fill_rainbow(h_LEDs, NUM_LEDS, initialHue += hueDensity, deltaHue);
-    // h_LEDs[0] = CRGB::Red;   // light LED 0
-    // lightFullStrip();
-    // MyFire();
-    // MyIceFire();
-    // PurpleR();
-    // UkrainFlag();
-    
-    if (ESP_BT.available()){
-			byte btRead = ESP_BT.read();
-			switch (btRead){
-				case 0:  clearLEDs(); break;
-        case 1:	 DrawPixels(h_LEDs[0], NUM_LEDS, CRGB::Green);  break;
-				case 2:	 fill_solid(h_LEDs, NUM_LEDS, CRGB::Plaid);  break;
-				case 3:	 firstLED();  break;
-				case 4:	 lightFullStrip();  break;
-				case 5:	 UkrainFlag();  break;
-				case 6:	 fill_rainbow(h_LEDs, NUM_LEDS, initialHue += hueDensity, deltaHue);  break;
-				case 7:	 Marquee();  break;
-				case 8:	 balls.Draw();  break;
-				case 9:	 DrawComet();  break;
-				case 10: DrawCometGfx(); break;
-				case 11: DrawComet3(); break;
-				case 12: DrawTwinkle(); break;
-				case 13: DrawPurpleTwinkle(); break;
-				case 14: MyFire(); break;
-				case 15: MyIceFire(); break;
-				case 16: PurpleR(); break;
-				default: nothingElseMatters();
-      }
-    }
+    // clearLEDs();
+    // Marquee();                             // see annotations in marquee.h for options
+    // DrawComet();                           // 
+    // DrawCometGfx();                        //
+    // DrawComet3();                          //
+    // Bounce();                              // Bouncing balls using BouncingBallEffect from bounce.h
+    // DrawMarquee();                         // multi color scrolling marquee effect
+    // fillGreen();                           // DrawPixels color = CRGB::Green
+    // DrawTwinkle();                         // multi color twinkle effect
+    // DrawPurpleTwinkle();                   // purple color palette twinkle effect
+    // Plaid();                               // solid fill color = CRGB::Plaid
+    // Rainbow();                             //
+    // firstLED();                            // light LED 0 CRGB::Red
+    // lightFullStrip();                      // solid fill color = CRGB::Red
+    // MyFire();                              // red palette fire effect
+    // MyIceFire();                           // blue palette fire effect
+    // PurpleR();                             // purple palette fire effect
+    // UkrainFlag();                          // 
 
-  //----------------------------------------------------------------------------------------------------
-
-  FastLED.setBrightness(h_Brightness);
-  FastLED.delay(10);                                         // Show and delay
+    FastLED.setBrightness(h_Brightness);      // set brightness
+    FastLED.delay(10);                        // Show and delay
 
   }
 
 }  // end of void loop just in case you missed it.
-
-// --------------------------------------------------------------------
-// 
-// --- User defined functions
-// 
-// --------------------------------------------------------------------
-
-void clearLEDs(){
-  FastLED.clear();
-};
-
-void firstLED(){
-  h_LEDs[0] = CRGB::Red;   // light LED 0
-};
-
-void Marquee(){
-  EVERY_N_MILLISECONDS(20){ DrawMarquee(); };
-};
-
-void MyIceFire(){
-    FastLED.clear();
-    ice.DrawIceFire();
-    FastLED.show(h_Brightness);
-};
-
-void MyFire(){
-    FastLED.clear();
-    fire.DrawFire();
-    FastLED.show(h_Brightness);
-};
-
-void PurpleR(){
-    FastLED.clear();
-    rain.DrawPurpleRain();
-    FastLED.show(h_Brightness);
-};
